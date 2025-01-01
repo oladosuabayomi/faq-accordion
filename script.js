f85bf200177883d57de25a87bf7d3d6882ff3975
oladosuabayomi <oladosuabayomi6@gmail.com>
2025-01-01 02:14:36 +0100

    Modified toggle functionality

--- script.js
+++ script.js
@@ -1,25 +1,78 @@
 'use strict';
 
-const closeIcon = document.querySelector('.minus');
-const openIcon = document.querySelector('.plus');
-const faqContent = document.querySelector('.faq-content');
+const btnHideContent = document.querySelectorAll('.minus');
+const btnShowContent = document.querySelectorAll('.plus');
+const faqContent = document.querySelectorAll('.faq-content');
 
-function toggleOpen() {
-    closeIcon.style.display = 'none';
-    openIcon.style.display = 'block'
-    faqContent.style.display = 'none'
+/* const hideContent = function () {
+    btnHideContent.classList.add('hide-element');
+    btnShowContent.classList.remove('hide-element')
+}
+ */
+
+for (let i = 0; i < btnShowContent.length; i++) {
+
+    const showContent = function () {
+        btnShowContent[i].classList.add('hidden')
+        btnHideContent[i].classList.remove('hidden');
+        faqContent[i].classList.remove('hidden')
+
+    }
+
+    btnShowContent[i].addEventListener('click', showContent);
 }
 
-function toggleClose() {
-    closeIcon.style.display = 'block';
-    openIcon.style.display = 'none'
-    faqContent.style.display = 'block'
+for (let i = 0; i < btnHideContent.length; i++) {
+
+    const hideContent = function () {
+        btnShowContent[i].classList.remove('hidden')
+        btnHideContent[i].classList.add('hidden');
+        faqContent[i].classList.add('hidden')
+
+    }
+
+    btnHideContent[i].addEventListener('click', hideContent);
 }
 
 
 
-closeIcon.addEventListener('click', toggleClose);
-openIcon.addEventListener('click', toggleOpen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toggleOpen() {
+//     closeIcon.style.display = 'none';
+//     openIcon.style.display = 'block'
+//     faqContent.style.display = 'none'
+// }
+
+// function toggleClose() {
+//     closeIcon.style.display = 'block';
+//     openIcon.style.display = 'none'
+//     faqContent.style.display = 'block'
+// }
+
+
+
+// closeIcon.addEventListener('click', toggleClose);
+// openIcon.addEventListener('click', toggleOpen);
 
 /* closeIcon.addEventListener('click', function () {
     closeIcon.style.display = 'none';
